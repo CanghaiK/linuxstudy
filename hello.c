6b4a91a8dc7f6dda9cacc3a351947cad2f79aadb
ZhangZhen <1290049069@qq.com>
2020-11-19 19:21:07 +0800

    first driver demo--hello-driver

---
+++ hello.c
@@ -0,0 +1,19 @@
+#include <linux/init.h>
+#include <linux/module.h>
+
+
+static int __init hello_init(void)
+{
+    printk(KERN_INFO "Enter : %s\n",__func__ );
+    return 0;
+}
+
+static void __exit hello_exit(void)
+{
+    printk(KERN_INFO "Enter :%s\n",__func__);
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+MODULE_AUTHOR("zhang zhen");
